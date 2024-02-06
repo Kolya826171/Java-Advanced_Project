5b85aa768aab2075e0c231a6ed87c2f0c30dda9f
kolya826171 <k.nirko53@gmail.com>
2024-02-06 16:03:25 +0200

    updated repository

--- admissions/src/main/java/project/dao/UserRepository.java
+++ admissions/src/main/java/project/dao/UserRepository.java
@@ -1,14 +1,18 @@
 package project.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import project.domain.User;
+import project.domain.UserRole;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 	Optional<User> findByEmail(String email);
+	
+	List<User> findByUserRole(UserRole role);
 }
